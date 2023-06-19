9e359d96c4f88bff4ccfe651d7c0c39cd73bc043
neon.mao <neon.mao@derbysoft.net>
2023-06-19 18:14:40 +0800

    update

---
+++ TransformerModel/pipeline.py
@@ -0,0 +1,56 @@
+from transformers import pipeline
+
+
+# text generation
+def text_generation():
+    generator = pipeline("text-generation", model="distilgpt2")
+    result = generator(
+        "In this course, we will teach you how to",
+        max_length=30,
+        num_return_sequences=2
+    )
+    print(result)
+
+
+def question_answering():
+    question_answer = pipeline("question-answering")
+    result = question_answer(question="Where do i work?",
+                             context="My name is maoqifan and i work at derby software in shanghai.")
+    print(result)
+
+
+def text_summarization():
+    summarizer = pipeline("summarization")
+    result = summarizer(
+        """
+        America has changed dramatically during recent years. Not only has the number of 
+        graduates in traditional engineering disciplines such as mechanical, civil, 
+        electrical, chemical, and aeronautical engineering declined, but in most of 
+        the premier American universities engineering curricula now concentrate on 
+        and encourage largely the study of engineering science. As a result, there 
+        are declining offerings in engineering subjects dealing with infrastructure, 
+        the environment, and related issues, and greater concentration on high 
+        technology subjects, largely supporting increasingly complex scientific 
+        developments. While the latter is important, it should not be at the expense 
+        of more traditional engineering.
+
+        Rapidly developing economies such as China and India, as well as other 
+        industrial countries in Europe and Asia, continue to encourage and advance 
+        the teaching of engineering. Both China and India, respectively, graduate 
+        six and eight times as many traditional engineers as does the United States. 
+        Other industrial countries at minimum maintain their output, while America 
+        suffers an increasingly serious decline in the number of engineering graduates 
+        and a lack of well-educated engineers.
+    """
+    )
+    print(result)
+
+
+def text_translation():
+    translator = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-en")
+    result = translator("Ce cours est produit par Hugging Face.")
+
+    print(result)
+
+if __name__ == '__main__':
+    text_translation()
